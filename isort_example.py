10ea0fd1371756b3d227b54475f545348662d2a1
shabanian2018 <mahdieh.shabanian@cchmc.org>
2023-01-09 14:04:59 +0000

    all pre commit example

---
+++ isort_example.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import (LinearRegression, LinearRegressionCV,
+                                  LogisticRegression, LogisticRegressionCV,
+                                  OrderedLogisticRegression)
+from sklearn.model_selection import train_test_split
+
+from flake8_example import very_long_function_name
